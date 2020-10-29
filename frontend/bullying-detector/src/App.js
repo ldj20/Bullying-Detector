5c7da5221809b3a39f9f236a726088ca8b14ff87
Lawrence Jiang <lawrencedjiang@gmail.com>
2020-10-28 23:45:53 -0400

    setup basic webpage structure

--- frontend/bullying-detector/src/App.js
+++ frontend/bullying-detector/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bullying Detector</h1>
+      <p class="message">Welcome! If you ever think that someone may be cyberbullying you, you can
+      copy messages or posts they made and paste them here. Please keep in mind that
+      this app is not perfect. Whether it tells you that you are being bullied or not,
+      your feelings are important. If you are worried that you are being bullied, the
+      best thing to do is talk to a trusted adult about it. You are not alone.</p>
+
+      <br/>
+      <h3>Paste Text Here</h3>
+        <form>
+        <textarea name="paragraph_text" cols="50" rows="13"></textarea>
+
+        </form>
     </div>
   );
 }
