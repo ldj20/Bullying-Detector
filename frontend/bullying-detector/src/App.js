805cdb7664f13d5ac21c1865927ab860ee5c37a1
Lawrence Jiang <lawrencedjiang@gmail.com>
2020-10-29 01:38:41 -0400

    styling

--- frontend/bullying-detector/src/App.js
+++ frontend/bullying-detector/src/App.js
@@ -3,8 +3,7 @@ import { useState } from 'react';
 function App() {
   
   const [text, setText] = useState("");
-  const [isHome, setIsHome] = useState(true)
-  const [render, setRender] = useState("bullied")
+  const [render, setRender] = useState("home")
 
   const handleInput = event => {
     setText(event.target.value);
@@ -16,31 +15,31 @@ function App() {
   }
   
   return (
-    isHome ? 
-      <div>
-        <h1>Bullying Detector</h1>
-        <p className="message">Welcome! If you ever think that someone may be cyberbullying you, you can
-        copy messages or posts they made and paste them here. Please keep in mind that
-        this app is not perfect. Whether it tells you that you are being bullied or not,
-        your feelings are important. If you are worried that you are being bullied, the
-        best thing to do is talk to a trusted adult about it. You are not alone.</p>
+    (render == "home") ? 
+      <div class="wrapper">
+        <header>
+          <h1>Bullying Detector</h1>
+          <p className="message">Welcome! If you ever think that someone may be cyberbullying you, you can
+          copy messages or posts they made and paste them here. Please keep in mind that
+          this app is not perfect. Whether it tells you that you are being bullied or not,
+          your feelings are important. If you are worried that you are being bullied, the
+          best thing to do is talk to a trusted adult about it. You are not alone.</p>
 
-        <p>If you want to learn how to handle getting bullyied, click this button.</p>
-        <button className="btn submit" onClick={() => setIsHome(false)}>Click Me!</button>
-        <br/>
+          <p>If you want to learn how to handle getting bullied, click this button.</p>
+          <button className="btn submit" onClick={() => setRender("bullied")}>Information</button>
+        </header>
         <h3>Paste Text Here</h3>
         <textarea name="paragraph_text" cols="50" rows="13" onChange={handleInput}></textarea>
         <br/>
         <button className="btn submit" onClick={submit}>Submit</button>
-      </div> :
-      render == "bullied" ?
-        <div>
+      </div> : (render == "bullied") ?
+        <div class="wrapper">
           <h1>Bullying Information</h1>
-          <button className="btn submit" onClick={() => setIsHome(true)}>Back To Homepage</button>
+          <button className="btn submit" onClick={() => setRender("home")}>Back To Homepage</button>
         </div> :
-        <div>
-          <h1>Encouragement Tab</h1>
-          <button className="btn submit" onClick={() => setIsHome(true)}>Back To Homepage</button>
+        <div class="wrapper">
+          <h1>Not bullied tab</h1>
+          <button className="btn submit" onClick={() => setRender("home")}>Back To Homepage</button>
         </div>
   );
 }
