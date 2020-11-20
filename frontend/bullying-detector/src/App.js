bd85f8c2c78e7b621f31c48ed5f12988a709f84c
Lawrence Jiang <lawrencedjiang@gmail.com>
2020-11-19 21:19:05 -0500

    a

--- frontend/bullying-detector/src/App.js
+++ frontend/bullying-detector/src/App.js
@@ -1,17 +1,38 @@
 import { useState } from 'react';
+const axios = require('axios');
+const {PredictionServiceClient} = require('@google-cloud/automl').v1;
 
 function App() {
-  
+
+  const projectId = "bullying-detector";
+  const location = "us-central1";
+  const modelId = "TCN4022003638799958016";
+  const scoreThreshold = "0.6";
+  const params = {score_threshold: scoreThreshold};
+
+  const client = new PredictionServiceClient();
+
   const [text, setText] = useState("");
-  const [render, setRender] = useState("home")
+  const [render, setRender] = useState("home");
+  const [prediction, setPrediction] = useState(); 
+
+  const predict = () => {
+    const request = {
+      name: client.modelPath(projectId, location, modelId),
+      payload: {
+        textSnippet: {
+          content: text,
+          mimeType: 'text/plain',
+        },
+      },
+    };
+    const [response] = client.predict(request);
+    setPrediction(response);
+    console.log(response);
+  }
 
   const handleInput = event => {
     setText(event.target.value);
-  }
-
-  const submit = () => {
-    //api call with text
-    return
   }
   
   return (
@@ -25,26 +46,32 @@ function App() {
           your feelings are important. If you are worried that you are being bullied, the
           best thing to do is talk to a trusted adult about it. You are not alone.</p>
 
-          <p>If you want to learn how to handle getting bullied, click this button.</p>
+          <p>If you want to learn how to handle bullies, click this button.</p>
           <button className="btn submit" onClick={() => setRender("bullied")}>Information</button>
         </header>
         <h3>Paste Text Here</h3>
         <textarea name="paragraph_text" cols="50" rows="13" onChange={handleInput}></textarea>
         <br/>
-        <button className="btn submit" onClick={submit}>Submit</button>
+        <button className="btn submit" onClick={predict}>Submit</button>
       </div> : (render == "bullied") ?
         <div class="wrapper">
           <header>
-            <h1>Bullying Information</h1>
+            <h1>Bullied Tab</h1>
           </header>
             <button className="btn submit" onClick={() => setRender("home")}>Back To Homepage</button>
-        </div> :
-        <div class="wrapper">
-          <header>
-            <h1>Not bullied tab</h1>
-          </header>
-          <button className="btn submit" onClick={() => setRender("home")}>Back To Homepage</button>
-        </div>
+        </div> : (render == "not bullied") ?
+          <div class="wrapper">
+            <header>
+              <h1>Not bullied tab</h1>
+            </header>
+            <button className="btn submit" onClick={() => setRender("home")}>Back To Homepage</button>
+          </div> :
+          <div class="wrapper">
+            <header>
+              <h1>Informational Tab</h1>
+            </header>
+            <button className="btn submit" onClick={() => setRender("home")}>Back To Homepage</button>
+          </div>
   );
 }
 
