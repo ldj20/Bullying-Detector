ee9156490a0037a261f20c6b1d9c38ee5ca31881
Lawrence Jiang <lawrencedjiang@gmail.com>
2020-11-23 14:12:55 -0500

    idk

--- server.js
+++ server.js
@@ -2,33 +2,37 @@ const express = require('express')
 const app = express();
 const {PredictionServiceClient} = require('@google-cloud/automl').v1;
 const cors = require("cors");
-const bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
+const bodyParser = require('body-parser');
+
+const jsonParser = bodyParser.json();
+const projectId = "bullying-detector";
+const location = "us-central1";
+const modelId = "TCN4022003638799958016";
+const client = new PredictionServiceClient();
+
 app.use(cors());
 
-app.post('/', jsonParser, (req, res) => {
-    const projectId = "bullying-detector";
-    const location = "us-central1";
-    const modelId = "TCN4022003638799958016";
-    const text = req.body.text;
-    const client = new PredictionServiceClient();
-    const request = {
-      name: client.modelPath(projectId, location, modelId),
-        payload: {
-          textSnippet: {
-            content: text,
-            mimeType: 'text/plain',
-          },
-        },
-    };
-    client.predict(request)
-      .then(response => {
-        console.log("here1");
-        res.send(response);
-      })
-      .catch(e => {
-        console.log(e);
-      })
+const ex = () => {}
+
+async function asyncFetch (req, res, next) {
+  const content = req.body.text;
+  const request = {
+    name: client.modelPath(projectId, location, modelId),
+    payload: {
+      textSnippet: {
+        content: content,
+        mimeType: 'text/plain', 
+      },
+    },
+  };
+  console.log(request);
+  const [response] = await client.predict(request)
+  req.data = response.json();
+  next();
+}
+
+app.post('/', jsonParser, asyncFetch, (req, res) => {
+  res.send(req.data);
 });
 
 app.listen(process.env.PORT || 8000, () => {
